e8d07b7b79c6d7c199c46474e7d71c1464c633e8
Kolby Boesel <kolbyboesel@Kolbys-MacBook-Pro.attlocal.net>
2024-12-10 16:58:28 -0600

    Connecting backend

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/weatherforecast') // Adjust based on your API route
+    axios.get('http://soapscores-dvbnchand2byhvhc.centralus-01.azurewebsites.net/weatherforecast') // Adjust based on your API route
       .then(response => setData(response.data))
       .catch(error => console.error(error));
   }, []);
