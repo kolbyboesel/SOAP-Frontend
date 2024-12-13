a6d54e321eb6206b7793378b3e80caede8fa3c34
Kolby Boesel <kolbyboesel@MacBookPro.attlocal.net>
2024-12-13 12:44:21 -0600

    Added team favorites to Favorites page

--- src/components/ScoreLive.js
+++ src/components/ScoreLive.js
@@ -2,16 +2,24 @@ import React from 'react';
 import '../styles/ConstructBoard.css';
 
 const ScoreLive = ({ eventData, date, time }) => {
+    const awayScore = eventData.awayScore.current;
+    const homeScore = eventData.homeScore.current;
+
     return (
         <div className='scoreboard-score'>
-            <div className="row team lose" style={{ borderLeft: 'none', borderTop: 'none', borderRight: 'none' }}>
+            <div
+                className={`row team ${awayScore > homeScore ? 'win' : 'lose'}`}
+                style={{ borderLeft: 'none', borderTop: 'none', borderRight: 'none' }}
+            >
                 <div className="team-name">{eventData.awayTeam.shortName}</div>
-                <div className="team-score">{eventData.awayScore.current}</div>
+                <div className="team-score">{awayScore}</div>
             </div>
 
-            <div className="row team">
+            <div
+                className={`row team ${homeScore > awayScore ? 'win' : 'lose'}`}
+            >
                 <div className="team-name">{eventData.homeTeam.shortName}</div>
-                <div className="team-score">{eventData.homeScore.current}</div>
+                <div className="team-score">{homeScore}</div>
             </div>
         </div>
     );
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -7,6 +7,7 @@ const Favorites = () => {
     const [isLoading, setIsLoading] = useState(true);
     const { userSettings } = useContext(UserSettingsContext);
     const [eventsData, setEventsData] = useState([]);
+    const [teamScoresData, setTeamScoresData] = useState([]);
     const [liveScoreMessage] = useState('Sign in to add your favorite leagues and view live scores here');
 
     // Function to fetch live events for each league
@@ -25,6 +26,21 @@ const Favorites = () => {
         }
     };
 
+    const fetchTeamScores = async (teamID, eventType) => {
+        try {
+            const response = await axios.get(
+                `https://soapscores-dvbnchand2byhvhc.centralus-01.azurewebsites.net/api/SofaScores/team-scores/${teamID}/${eventType}`
+            );
+            if (response.status === 200) {
+                setTeamScoresData((prevData) => [...prevData, ...response.data]);
+            } else {
+                console.log(`No team events found for ${eventType} in team ${teamID}`);
+            }
+        } catch (error) {
+            console.error('Error fetching team scores:', error);
+        }
+    };
+
     // Fetch events for each favorite league
     useEffect(() => {
         const fetchAllEvents = async () => {
@@ -37,15 +53,21 @@ const Favorites = () => {
                 });
             }
 
+            if (userSettings.TeamFavorites && userSettings.TeamFavorites.length > 0) {
+                userSettings.TeamFavorites.forEach((team) => {
+                    promises.push(fetchTeamScores(team.teamID, 'next'));
+                    promises.push(fetchTeamScores(team.teamID, 'last'));
+                });
+            }
+
             await Promise.all(promises);
 
             setIsLoading(false);
         };
 
         fetchAllEvents();
-    }, [userSettings.LeagueFavorites]);
+    }, [userSettings.LeagueFavorites, userSettings.TeamFavorites]);
 
-    // Group events by tournament name
     const groupedEvents = eventsData.reduce((acc, event) => {
         const tournamentName = event.tournament.uniqueTournament.name;
         if (!acc[tournamentName]) {
@@ -55,7 +77,27 @@ const Favorites = () => {
         return acc;
     }, {});
 
-    // Function to select events, prioritizing live past events and filling with future events
+    const groupedTeamScores = teamScoresData.reduce((acc, event) => {
+        const awayTeamName = event.awayTeam.name;
+        const homeTeamName = event.homeTeam.name;
+
+        if (userSettings.TeamFavorites.some(team => team.teamID === event.homeTeam.id)) {
+            if (!acc[homeTeamName]) {
+                acc[homeTeamName] = [];
+            }
+            acc[homeTeamName].push(event);
+        }
+
+        if (userSettings.TeamFavorites.some(team => team.teamID === event.awayTeam.id)) {
+            if (!acc[awayTeamName]) {
+                acc[awayTeamName] = [];
+            }
+            acc[awayTeamName].push(event);
+        }
+
+        return acc;
+    }, {});
+
     const selectEvents = (events) => {
         const pastEvents = events.filter(event => event.status.type === 'inprogress');
         const nextEvents = events.filter(event => event.status.type === 'notstarted');
@@ -64,7 +106,6 @@ const Favorites = () => {
 
         selectedEvents.push(...pastEvents);
 
-        // If there are fewer than 5 events, fill the remaining spots with upcoming events
         const remainingCount = 5 - selectedEvents.length;
         if (remainingCount > 0) {
             selectedEvents.push(...nextEvents.slice(0, remainingCount));
@@ -73,51 +114,104 @@ const Favorites = () => {
         return selectedEvents.slice(0, 5);
     };
 
+    const selectOneEventForTeam = (events) => {
+        const liveEvent = events.find(event => event.status.type === 'inprogress');
+        if (liveEvent) {
+            return liveEvent;
+        }
+
+        const nextEvent = events.find(event => event.status.type === 'notstarted');
+        if (nextEvent) {
+            return nextEvent;
+        }
+
+        return events[0];
+    };
+
+    const selectedEvents = Object.keys(groupedTeamScores).map((teamName) => {
+        const events = groupedTeamScores[teamName];
+        const selectedEvent = selectOneEventForTeam(events);
+        return {
+            teamName,
+            event: selectedEvent
+        };
+    });
+
+    const sortedSelectedEvents = [...selectedEvents].sort((a, b) => {
+        const timestampA = new Date(a.event.startTimestamp).getTime();
+        const timestampB = new Date(b.event.startTimestamp).getTime();
+
+        return timestampA - timestampB;
+    });
+
     return (
         <div>
             {isLoading ? (
                 <div className="loading liveLoading">Loading&#8230;</div>
             ) : (
                 <div className="scroll-view pt-3 pb-5" id='top-favorites'>
-                    {Object.keys(groupedEvents).length > 0 ? (
+                    {sortedSelectedEvents.length > 0 ? (
                         <div className="league-events-container">
-                            <div className="events-section">
-                                {/* Iterate through each grouped tournament */}
-                                {Object.keys(groupedEvents).map((tournamentName, index) => {
-                                    const tournamentEvents = groupedEvents[tournamentName];
+                            <div className="league-container">
+                                <h4>Favorites</h4>
+                                <div className="events">
+                                    {sortedSelectedEvents.map(({ teamName, event }, index) => {
+                                        // Check if this is the last event
+                                        const isLastEvent = index === sortedSelectedEvents.length - 1;
 
-                                    // Select 5 events based on the logic above
-                                    const selectedEvents = selectEvents(tournamentEvents);
-
-                                    return (
-                                        <div key={index} className="league-container">
-                                            <h4>{tournamentName}</h4>
-                                            <div className="events">
-                                                {selectedEvents.map((event, i) => {
-                                                    const isLastEvent = i === selectedEvents.length - 1;
-
-                                                    return (
-                                                        <div
-                                                            key={i}
-                                                            className={`event-board ${isLastEvent ? '' : 'bottom-border'}`}
-                                                        >
-                                                            <ConstructBoard EventData={event} />
-                                                        </div>
-                                                    );
-                                                })}
+                                        return (
+                                            <div
+                                                key={index}
+                                                className={`event-board ${isLastEvent ? '' : 'bottom-border'}`}
+                                            >
+                                                <ConstructBoard EventData={event} />
                                             </div>
-                                        </div>
-                                    );
-                                })}
+                                        );
+                                    })}
+                                </div>
                             </div>
                         </div>
                     ) : (
-                        <div className="row league-container no-gutters score-container-scroll xs-padding overflow-auto liveScoresContainer">
-                            <div className="page-text text-center">
-                                {liveScoreMessage}
-                            </div>
+                        <div className="no-events-message">
+                            <div className="page-text text-center">{liveScoreMessage}</div>
                         </div>
                     )}
+
+                    {Object.keys(groupedEvents).length > 0 ? (
+                        <div className="league-events-container">
+                            <div className="events-section">
+                                {Object.keys(groupedEvents).map((tournamentName, index) => {
+                                    const tournamentEvents = groupedEvents[tournamentName];
+
+                                    const selectedEvents = selectEvents(tournamentEvents);
+
+                                    if (selectedEvents.length > 0) {
+                                        return (
+                                            <div key={index} className="league-container">
+                                                <h4>{tournamentName}</h4>
+                                                <div className="events">
+                                                    {selectedEvents.map((event, i) => {
+                                                        const isLastEvent = i === selectedEvents.length - 1;
+
+                                                        return (
+                                                            <div
+                                                                key={i}
+                                                                className={`event-board ${isLastEvent ? '' : 'bottom-border'}`}
+                                                            >
+                                                                <ConstructBoard EventData={event} />
+                                                            </div>
+                                                        );
+                                                    })}
+                                                </div>
+                                            </div>
+                                        );
+                                    }
+
+                                    return null;
+                                })}
+                            </div>
+                        </div>
+                    ) : null}
                 </div>
             )}
 
